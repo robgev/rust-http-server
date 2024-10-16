39bb2563c11ac427adeb6063f0e441a265cfb1d8
Robert Gevorgyan <robert1999.g@gmail.com>
2024-10-16 20:24:35 +0400

    add a echo route

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
-use std::io::{Write, Read};
+use std::{io::{Write, Read}, net::TcpStream, fmt::format};
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    // println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
+                // URL Extractor
                 let mut buf = [0; 512];
                 let _ = _stream.read(&mut buf);
                 let request = String::from_utf8(buf.to_vec()).unwrap();
@@ -18,15 +16,33 @@ fn main() {
                 let path_end = request.find(" HTTP").unwrap();
                 let resource = &request[path_start..path_end];
 
-                if resource == "" {
-                    let _ = _stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
-                } else {
-                    let _ = _stream.write_all("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes());
-                }
+                // Server - <Resource URL> -> <Server Response>
+                server(resource, _stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    // fn create_route(route: String, handler: Fun) -> Route
+    //
+    // fn router(routes: Route[], url: String) {
+    //  if routes.route matches url {
+    //      route.handler();
+    //  }
+    // }
+
+    fn server(resource: &str, mut stream: TcpStream) {
+        if resource == "" {
+            let _ = stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
+        } else if let Some(match_start) = resource.find("echo/") {
+            let query_start = match_start + "echo/".len();
+            let echo_text = &resource[query_start..];
+            let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", echo_text.len(), echo_text);
+            let _ = stream.write_all(response.as_bytes());
+        } else {
+            let _ = stream.write_all("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes());
+        }
+    }    
 }
